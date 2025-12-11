b62ca742e2ff5c6bec1ccf72a544e699935ea607
Ryan Harrison <ryan.harrison@endava.com>
2025-12-11 16:30:40 -0500

    Remove firmware/build from git tracking

--- firmware/main/audio_stream.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-void audio_stream_task(void *pvParameters);
--- firmware/main/wifi.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "esp_err.h"
-
-/**
- * @brief Initialize Wi-Fi in station mode and connect using sdkconfig credentials.
- */
-esp_err_t wifi_init_sta(void);
